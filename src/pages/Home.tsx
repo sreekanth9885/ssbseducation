7f9f0cc089bffe5d1d44356bcd14d7d531f86c3c
Srikanth Bethi <srikanth.bethi01@gmail.com>
2026-03-29 13:57:04 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,17 @@
 // src/pages/Home.tsx
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import Hero from "../components/home/Hero";
 import Stats from "../components/home/Stats";
-import { getHomeContent } from "../services/api";
+// import { getHomeContent } from "../services/api";
 
 const Home = () => {
-  const [data, setData] = useState<any>(null);
+  // const [data, setData] = useState<any>(null);
 
-  useEffect(() => {
-    getHomeContent()
-      .then((res) => setData(res.data))
-      .catch(() => console.log("API not connected yet"));
-  }, []);
+  // useEffect(() => {
+  //   getHomeContent()
+  //     .then((res) => setData(res.data))
+  //     .catch(() => console.log("API not connected yet"));
+  // }, []);
 
   return (
     <div>
