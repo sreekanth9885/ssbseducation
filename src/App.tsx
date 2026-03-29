9aac06890619c52b790b5db8f0ea6e1473ecfcb0
Srikanth Bethi <srikanth.bethi01@gmail.com>
2026-03-29 12:38:39 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <h1>Welcome to SSBS Education</h1>
       <h1>We are one of the top educational institutions in the country</h1>
-      <h2>developed by Radiant Software Solutions</h2>
+      <h2>developed by Radiant Software Solutions Srikanth bethi</h2>
     </>
   )
 }
