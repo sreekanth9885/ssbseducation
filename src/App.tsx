a80d4eb70c48bbe3993933e50d60ad80f8860d11
Srikanth Bethi <srikanth.bethi01@gmail.com>
2026-03-29 12:15:17 +0530

    ssbs eduction

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Welcome to SSBS Education</h1>
+      <h1>We are one of the top educational institutions in the country</h1>
+    </>
+  )
+}
+
+export default App
